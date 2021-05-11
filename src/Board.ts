45bc0f188995eec8d1594f5991e371aa73c5a9d1
Simon Yoo <simonyoo@Simonui-MacBookPro.local>
2021-05-12 02:51:30 +0900

    second add

--- src/Board.ts
+++ src/Board.ts
@@ -1,4 +1,5 @@
 import {Piece} from './Piece';
+import { Player } from './Player';
 
 export interface Position {
     row: number;
@@ -30,7 +31,7 @@ export class Cell{
     }
 
     render() {
-        if(this.isActice) {
+        if (this.isActice) {
             this._el.classList.add('active');
         }else {
             this._el.classList.remove('active');
@@ -42,7 +43,9 @@ export class Cell{
 export class Board {
     cells: Cell[] = [];
     _el: HTMLElement = document.createElement('div');
-    constructor() {
+    map:WeakMap<HTMLElement, Cell> = new WeakMap();
+
+    constructor(upperPlayer: Player, lowerPlayer: Player) {
         this._el.className = 'board';
 
         for (let row = 0; row < 4; row++) {
@@ -51,7 +54,13 @@ export class Board {
             this._el.appendChild(rowEl);
 
             for(let col = 0; col < 3; col++) {
-                const cell = new Cell({row, col}, null);
+                const piece = 
+                    upperPlayer.getPieces().find(({ currentPosition }) => 
+                        currentPosition.col === col && currentPosition.row === row) || 
+                    lowerPlayer.getPieces().find(({ currentPosition }) => 
+                        currentPosition.col === col && currentPosition.row === row) 
+                const cell = new Cell({row, col}, piece);
+                this.map.set(cell._el, cell);
                 this.cells.push(cell);
                 rowEl.appendChild(cell._el);
             }
